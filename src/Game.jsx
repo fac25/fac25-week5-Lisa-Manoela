f51512f7466f29b5f53f76ef6928043f651306d1
lisahns <lisa.hanes@hotmail.de>
2022-10-19 14:07:51 +0100

    add eventlistener to tiles and compare tiles

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,3 +1,5 @@
+import {useEffect} from 'react'
+
 const levels = {
   "level1": 8,
   "level2": 10,
@@ -5,6 +7,13 @@ const levels = {
   "level4": 15,
   "level5": 18
 }
+ /*
+  4X4
+  4X5
+  4x6
+  5x6
+  6x6
+*/
 
 function arrayOfAscendingNums (n) {
   let newArray = [];
@@ -16,19 +25,19 @@ function arrayOfAscendingNums (n) {
   return newArray;
 }
 
-
-function Game({level}) {
+function Game({level, tileValue, setTileValue}) {
  
- /*
-  4X4
-  4X5
-  4x6
-  5x6
-  6x6
-*/
-
+  useEffect(() => {
+    const memoryGame = document.querySelector(".memoryGame")
+    memoryGame.addEventListener("mouseclick", compareTiles);
+    return memoryGame.removeEventListener("mouseclick", compareTiles) 
+  }, []);
+  
+  function compareTiles(e) {
+    setTileValue([e.target.id])
+  }
     const numOfCardsForCurrentLevel =  levels["level" + level]
-    console.log(numOfCardsForCurrentLevel)
+    // console.log(numOfCardsForCurrentLevel)
 
     const uniqueCards = arrayOfAscendingNums(numOfCardsForCurrentLevel)
     const gameBoard = uniqueCards.concat(uniqueCards)
@@ -37,11 +46,18 @@ function Game({level}) {
     const randomisedBoard = gameBoard.sort( () => .5 - Math.random() )
 
     // [ 2, 0, 3, 1, 0, 4, 4, 1, 3, 2 ]
+function RandomiseBoard(){
+  const board = randomisedBoard.map(
+    (tile, index) => {
+  return (<li className="tile" /*onClick={compareTiles}*/ key={index} id={"tile"+tile} ><h2>{tile}</h2></li>)
+});
+  return board 
+}
 
   return (
     <div>
         <ul className="memoryGame">
-            {randomisedBoard.map((tile) => (<li className="tile" id={"tile"+tile} ><h2>{tile}</h2></li>))}
+          <RandomiseBoard/>
         </ul>
     </div>
   )
