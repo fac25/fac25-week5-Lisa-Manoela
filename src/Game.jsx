1e3e47c626592751ba5f042219d3af949c1d4d70
Manoela <mo.tasheva@gmail.com>
2022-10-19 17:06:03 +0100

    Co-authored-by: Lisa <lisahns@users.noreply.github.com> Co-authored-by: yassienAbdillahi <yassienAbdillahi@users.noreply.github.com>

--- src/Game.jsx
+++ src/Game.jsx
@@ -15,82 +15,84 @@ const levels = {
   6x6
 */
 // CREATE BOARD
-function arrayOfAscendingNums (n) {
-  let newArray = [];
 
-  for (let i = 0; i < n; i++) {
-    newArray.push(i)
-  }
 
-  return newArray;
-}
-function createBoard(level) {
-  const numOfCardsForCurrentLevel =  levels["level" + level]
-    // console.log(numOfCardsForCurrentLevel)
-  const uniqueCards = arrayOfAscendingNums(numOfCardsForCurrentLevel)
-  const gameBoard = uniqueCards.concat(uniqueCards)
-    // Randomise
-    const randomisedBoard = gameBoard.sort( () => .5 - Math.random() )
-  
-    const board = randomisedBoard.map(
-      (tile, index) => {
-    return (<li className="tile" onClick={compareTiles} key={index} id={"tile"+index} >{tile}</li>)
-  });
-    return board 
-  }
-  
-  const board = createBoard(1)
-
-  // PLAY GAME
-  let clicked = [];
-  let match;
-  let clickedIds = []
-  let points = 0
-  let guesses = 0
-  let score = 0
-  
-  function compareTiles(e) {
-
-  clicked.push(e.target.innerText);
-  clickedIds.push(e.target.id)
-
-  if (clicked.length === 2 && clickedIds[0] !== clickedIds[1]) {
-    guesses++
-
-    match = clicked[0] === clicked[1]? true: false;
-    clicked = []
-    
-    // If it's a match, hide the cards
-    match && clickedIds.forEach((id) => {document.getElementById(id).style.display = "none"}) && points++
-    clickedIds = []
-  } 
-}
 
 // COMPONENT
 function Game({level}) {
-  // const [points, setPoints] = useState(0)
-  
-  // useEffect(() => {
-  //   const memoryGame = document.querySelectorAll(".indTile")
-  //   memoryGame.forEach((indTile) => {indTile.addEventListener("click", console.log("test"))});
-  //   return memoryGame.forEach((indTile) => {indTile.removeEventListener("click", compareTiles) });
-  // }, []);
+  const [points, setPoints] = useState(0)
 
- 
-  useEffect(() =>{
-    const pointsEl = document.getElementById("points")
-    console.log(pointsEl)
-  }, [points])
+  function arrayOfAscendingNums (n) {
+    let newArray = [];
+  
+    for (let i = 0; i < n; i++) {
+      newArray.push(i)
+    }
+  
+    return newArray;
+  }
+  function createBoard(level, newGame) {
+    const numOfCardsForCurrentLevel =  levels["level" + level]
+      // console.log(numOfCardsForCurrentLevel)
+    const uniqueCards = arrayOfAscendingNums(numOfCardsForCurrentLevel)
+    const gameBoard = uniqueCards.concat(uniqueCards)
+      // Randomise
+       const randomisedBoard = gameBoard.sort( () => .5 - Math.random() )
     
+      const board = randomisedBoard.map(
+        (tile, index) => {
+      return (<li className="tile" onClick={compareTiles} key={index} id={"tile"+index} >{tile}</li>)
+    });
+      return board 
+    }
+    
+
+    // PLAY GAME
+    let clicked = [];
+    let match;
+    let clickedIds = []
+    let guesses = 0
+    let score = 0
+    
+    function compareTiles(e) {
+  
+    clicked.push(e.target.innerText);
+    clickedIds.push(e.target.id)
+  
+    if (clicked.length === 2 && clickedIds[0] !== clickedIds[1]) {
+      guesses++
+  
+      match = clicked[0] === clicked[1]? true: false;
+      clicked = []
+      
+      // If it's a match, hide the cards
+      match && clickedIds.forEach((id) => {document.getElementById(id).style.display = "none"; setPoints(points + 1)})
+      clickedIds = []
+    } 
+    return match
+  }
+
+  const board = createBoard(1)
+
     // [ 2, 0, 3, 1, 0, 4, 4, 1, 3, 2 ]
+function Info({points}) {
 
   return (
-    <div>
-      <span>
-        <p id="points">{points}</p>
+    <span>
+        <p>Points: <span id="points">{points}</span></p>
         <p>Guesses {guesses}</p>
         <p>Score {score}</p>
       </span>
+  )
+}
+
+ useEffect(() => {
+    document.getElementById("points").innerText=points;
+  }, [points])
+
+  return (
+    <div>
+      <Info points={points}/>
         <ul className="memoryGame">
           {board}
         </ul>
